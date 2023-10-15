8d1810a0906e30f8c01ffa91cf79796c576f4d55
Shazid <shazid.dev@gmail.com>
2023-10-15 19:46:18 +0600

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "d-bg": "#141C2F",
+      "d-bg-atl": "#1F2A48",
+      "d-text": "#8E94A4",
+      blue: "#0079FE",
+      white: "#FEFEFE",
+      red: "#F74646",
+      "l-bg": "#F5F8FF",
+      "l-heading": "#2F3338",
+      "l-text": "#A2B0C7",
+    },
   },
   plugins: [],
 };
