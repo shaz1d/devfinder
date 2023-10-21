325b038cc5e89f7bc4ccdc5eb43669f7ac57dff4
Shazid <shazid.dev@gmail.com>
2023-10-21 13:45:13 +0600

    section added

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,10 +1,14 @@
 import userAvater from "../../assets/user.png";
+import locationIcon from "../../assets/icon-location.svg";
+import twitterIcon from "../../assets/icon-twitter.svg";
+import linkIcon from "../../assets/icon-url.svg";
+import companyIcon from "../../assets/icon-company.svg";
 const Result = () => {
   return (
     <div className="p-8 rounded-[15px] bg-cbg">
-      <div className="flex gap-5 mb-7">
+      <div className="flex gap-5 mb-6">
         <img
-          className=" w-24 h-24 object-cover rounded-full"
+          className=" w-24 h-24 object-cover rounded-full sm:hidden"
           src={userAvater}
           alt="img"
         />
@@ -32,6 +36,32 @@ const Result = () => {
         <div>
           <p className="text-sm">Following</p>
           <h3 className="font-bold">8</h3>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-5 mt-7">
+        <div className="flex flex-col gap-5">
+          <a className="flex items-center gap-2" href="">
+            {" "}
+            <img src={locationIcon} alt="" />
+            <span>San Fransisco</span>
+          </a>
+          <a className="flex items-center gap-2" href="">
+            {" "}
+            <img src={linkIcon} alt="" />
+            <span>San Fransisco</span>
+          </a>
+        </div>
+        <div className="flex flex-col gap-5">
+          <a className="flex items-center gap-2" href="">
+            {" "}
+            <img src={twitterIcon} alt="" />
+            <span>San Fransisco</span>
+          </a>
+          <a className="flex items-center gap-2" href="">
+            {" "}
+            <img src={companyIcon} alt="" />
+            <span>San Fransisco</span>
+          </a>
         </div>
       </div>
     </div>
