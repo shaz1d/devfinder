87fddad6257ba012c4792efbcdf2f2989918b2b8
Shazid <shazid.dev@gmail.com>
2023-10-22 20:01:26 +0600

    c

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -6,62 +6,73 @@ import companyIcon from "../../assets/icon-company.svg";
 const Result = () => {
   return (
     <div className="p-8 rounded-[15px] bg-cbg">
-      <div className="flex gap-5 mb-6">
-        <img
-          className=" w-24 h-24 object-cover rounded-full sm:hidden"
-          src={userAvater}
-          alt="img"
-        />
-        <div className="flex flex-col flex-grow gap-2 sm:flex-row sm:justify-between">
-          <div>
-            <h2 className="font-bold text-2xl">Shazidul Islam</h2>
-            <p className=" text-accent">@octocat</p>
+      <div className="flex gap-5">
+        <div className=" shrink-0">
+          <img
+            className=" w-24 h-24 object-cover rounded-full hidden sm:block"
+            src={userAvater}
+            alt="img"
+          />
+        </div>
+        <div>
+          <div className="flex gap-5 mb-6">
+            <img
+              className=" w-24 h-24 object-cover rounded-full sm:hidden"
+              src={userAvater}
+              alt="img"
+            />
+            <div className="flex flex-col flex-grow gap-2 sm:flex-row sm:justify-between">
+              <div>
+                <h2 className="font-bold text-2xl">Shazidul Islam</h2>
+                <p className=" text-accent">@octocat</p>
+              </div>
+              <p>Joined 25 Jan 2011</p>
+            </div>
           </div>
-          <p>Joined 25 Jan 2011</p>
-        </div>
-      </div>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam nemo
-        maxime modi atque, quasi nam!
-      </p>
-      <div className="py-4 px-6 rounded-[12px] bg-abg flex justify-between text-center mt-5">
-        <div>
-          <p className="text-sm">Repos</p>
-          <h3 className="font-bold">8</h3>
-        </div>
-        <div>
-          <p className="text-sm">Followers</p>
-          <h3 className="font-bold">3435</h3>
-        </div>
-        <div>
-          <p className="text-sm">Following</p>
-          <h3 className="font-bold">8</h3>
-        </div>
-      </div>
-      <div className="grid grid-cols-2 gap-5 mt-7">
-        <div className="flex flex-col gap-5">
-          <a className="flex items-center gap-2" href="">
-            {" "}
-            <img src={locationIcon} alt="" />
-            <span>San Fransisco</span>
-          </a>
-          <a className="flex items-center gap-2" href="">
-            {" "}
-            <img src={linkIcon} alt="" />
-            <span>San Fransisco</span>
-          </a>
-        </div>
-        <div className="flex flex-col gap-5">
-          <a className="flex items-center gap-2" href="">
-            {" "}
-            <img src={twitterIcon} alt="" />
-            <span>San Fransisco</span>
-          </a>
-          <a className="flex items-center gap-2" href="">
-            {" "}
-            <img src={companyIcon} alt="" />
-            <span>San Fransisco</span>
-          </a>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            nemo maxime modi atque, quasi nam!
+          </p>
+          <div className="py-4 px-6 rounded-[12px] bg-abg flex justify-between text-center mt-5">
+            <div>
+              <p className="text-sm">Repos</p>
+              <h3 className="font-bold">8</h3>
+            </div>
+            <div>
+              <p className="text-sm">Followers</p>
+              <h3 className="font-bold">3435</h3>
+            </div>
+            <div>
+              <p className="text-sm">Following</p>
+              <h3 className="font-bold">8</h3>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-5 mt-7">
+            <div className="flex flex-col gap-5">
+              <a className="flex items-center gap-2" href="">
+                {" "}
+                <img src={locationIcon} alt="" />
+                <span>San Fransisco</span>
+              </a>
+              <a className="flex items-center gap-2" href="">
+                {" "}
+                <img src={linkIcon} alt="" />
+                <span>San Fransisco</span>
+              </a>
+            </div>
+            <div className="flex flex-col gap-5">
+              <a className="flex items-center gap-2" href="">
+                {" "}
+                <img src={twitterIcon} alt="" />
+                <span>San Fransisco</span>
+              </a>
+              <a className="flex items-center gap-2" href="">
+                {" "}
+                <img src={companyIcon} alt="" />
+                <span>San Fransisco</span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
