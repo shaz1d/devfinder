cd7f105b9acf5367f9695d6184aa6029eebffc02
Shazid <shazid.dev@gmail.com>
2023-11-01 19:27:41 +0600

    updated dist added

--- src/components/Container/Container.tsx
+++
@@ -1,8 +0,0 @@
-type ContainerProps = {
-  children: React.ReactNode;
-};
-const Container = ({ children }: ContainerProps) => {
-  return <div className="w-full max-w-2xl mx-auto">{children}</div>;
-};
-
-export default Container;
